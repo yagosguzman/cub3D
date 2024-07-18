fc62c6d66443cd1acf9a7415700f73a6140b8335
ysanchez <yagosguzman@gmail.com>
2024-07-18 21:17:30 +0200

    tweak

--- src/cast_rays.c
+++ src/cast_rays.c
@@ -6,7 +6,7 @@
 /*   By: ysanchez <ysanchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 19:37:18 by gpinilla          #+#    #+#             */
-/*   Updated: 2024/07/18 21:14:17 by ysanchez         ###   ########.fr       */
+/*   Updated: 2024/07/18 21:14:48 by ysanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,6 @@ void	cast_rays(t_game *game)
 		draw_walls(game, ray, x);
 		free(ray);
 	}
-	mlx_put_image_to_window(game->mlx->mlx, game->mlx->win,
 	mlx_put_image_to_window(game->mlx->mlx, game->mlx->win,
 		game->mlx->img, 0, 0);
 }
